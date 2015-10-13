18688e3c8dad5346637a6e3b731886925bfb1883
monthonrose <monthonspy@hotmail.com>
2015-10-13 22:33:48 +0700

    edit file

---
+++ MAMTv1/app/src/main/java/com/fatel/mamtv1/AlarmFragment.java
@@ -0,0 +1,92 @@
+package com.fatel.mamtv1;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AlarmFragment extends android.support.v4.app.Fragment {
+
+
+    public AlarmFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_alarm, container, false);
+        createSpinner(12, R.id.start_hr,true,view);
+        createSpinner(60, R.id.start_min,false,view);
+        createSpinner(12, R.id.fin_hr,true,view);
+        createSpinner(60, R.id.fin_min,false,view);
+        createSpinnerAmPm(R.id.start_AP,view);
+        createSpinnerAmPm(R.id.fin_AP,view);
+        createSpinnerFrq(R.id.frq_min,view);
+
+        return view;
+    }
+
+    public void createSpinner(int num,int id,boolean isHr,View view)
+    {
+        Spinner spinner = (Spinner)view.findViewById(id);
+        String[] numm = new String[num];
+        ArrayAdapter<String> adapter;
+        if(isHr) {
+            for (int i = 1; i <= num; i++) {
+                if(i<10)
+                    numm[i - 1] = "0" + i;
+                else
+                    numm[i - 1] = "" + i;
+            }
+            adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, numm);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+            spinner.setSelection(11);
+        }
+        else {
+            for (int i = 0; i < num; i++) {
+                if(i<10)
+                    numm[i] = "0" + i;
+                else
+                    numm[i] = "" + i;
+            }
+            adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, numm);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        }
+
+    }
+
+    public void createSpinnerAmPm(int id,View view)
+    {
+        Spinner spinner = (Spinner)view.findViewById(id);
+        String[] numm = new String[]{"AM","PM"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, numm);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void createSpinnerFrq(int id,View view)
+    {
+        Spinner spinner = (Spinner)view.findViewById(id);
+        String[] numm = new String[]{"15","30","45","60","75","90"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, numm);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+
+
+
+}
---
+++ MAMTv1/app/src/main/java/com/fatel/mamtv1/DBAlarmHelper.java
@@ -0,0 +1,42 @@
+package com.fatel.mamtv1;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Monthon on 12/10/2558.
+ */
+public class DBAlarmHelper extends SQLiteOpenHelper{
+    private final String TAG = getClass().getSimpleName();
+    private SQLiteDatabase sqLiteDatabase;
+
+    public DBAlarmHelper(Context context){
+        super(context,"fatel_alarm.db",null,DatabaseAlarm.DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String CREATE_ALRAM_TABLE = String.format("CREATE TABLE %s " +
+                        "(%s INTEGER PRIMARY KEY  AUTOINCREMENT, %s TEXT, %s TEXT, %s TEXT, %s TEXT, " +
+                        "%s TEXT,%s TEXT,%s TEXT,%s TEXT)",
+                DatabaseAlarm.TABLE,
+                DatabaseAlarm.Column.ID,
+                DatabaseAlarm.Column.START_HR,
+                DatabaseAlarm.Column.START_MIN,
+                DatabaseAlarm.Column.STOP_HR,
+                DatabaseAlarm.Column.STOP_MIN,
+                DatabaseAlarm.Column.START_INTERVAL,
+                DatabaseAlarm.Column.STOP_INTERVAL,
+                DatabaseAlarm.Column.FRQ,
+                DatabaseAlarm.Column.DAY);
+        Log.i(TAG,CREATE_ALRAM_TABLE);
+        db.execSQL(CREATE_ALRAM_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+        String DROP_ALRAM_TABLE = "DROP TABLE IF EXISTS"+DatabaseAlarm.TABLE;
+        db.execSQL(DROP_ALRAM_TABLE);
+        Log.i(TAG,"Upgrade Database from "+oldVersion+" to "+newVersion);
+        onCreate(db);
+    }
+}
