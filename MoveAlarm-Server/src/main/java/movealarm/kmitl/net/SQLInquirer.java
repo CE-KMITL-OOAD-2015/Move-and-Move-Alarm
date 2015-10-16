67739db6402eb9e96078f9ba619c6ea0a427d1bd
oat9002 <oat9002@hotmail.com>
2015-10-16 11:39:07 +0700

    [#8]create find method.

--- MoveAlarm-Server/src/main/java/movealarm/kmitl/net/SQLInquirer.java
+++ MoveAlarm-Server/src/main/java/movealarm/kmitl/net/SQLInquirer.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 
 public class SQLInquirer {
 
-    private String sqlCommand;
     private Connection connector;
     private Statement stmt;
     private ResultSet rs;
@@ -23,20 +22,42 @@ public class SQLInquirer {
         collection = new ArrayList<HashMap<String, Object>>();
     }
 
-    public boolean startQuery()
-    {
-        try {
-            stmt = connector.createStatement();
-            rs = stmt.executeQuery(sqlCommand);
-        } catch (SQLException e) {
-            return false;
+    public ArrayList<HashMap<String, Object>> where(String tableName, String colName, String operator, String value) throws SQLException {
+        rs = stmt.executeQuery("SELECT * FROM " + tableName + " WHERE " + colName + " " + operator + " '" + value + "'");
+        ResultSetMetaData metaData = rs.getMetaData();
+        while(rs.next()) {
+            HashMap<String, Object> temp = new HashMap<String, Object>();
+            for(int i = 1; i <= metaData.getColumnCount(); i++)
+                temp.put(metaData.getColumnName(i), rs.getObject(i));
+            collection.add(temp);
         }
-        return true;
+        return collection;
     }
 
-    public void setCommand(String sqlCommand)
-    {
-        this.sqlCommand = sqlCommand;
+    public ArrayList<HashMap<String, Object>> where(String sqlCommand) throws SQLException {
+        rs = stmt.executeQuery(sqlCommand);
+        ResultSetMetaData metaData = rs.getMetaData();
+        while(rs.next()) {
+            HashMap<String, Object> temp = new HashMap<String, Object>();
+            for(int i = 1; i <= metaData.getColumnCount(); i++)
+                temp.put(metaData.getColumnName(i), rs.getObject(i));
+            collection.add(temp);
+        }
+        return collection;
+    }
+
+    public String testQuery()  {
+        String word = "test";
+        try {
+            if(isConnecting()) {
+                rs = stmt.executeQuery("SELECT * FROM testTable");
+                rs.next();
+                word = rs.getString(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return word;
     }
 
     public boolean startConnection()
@@ -44,8 +65,9 @@ public class SQLInquirer {
         try{
             Class.forName("org.mariadb.jdbc.Driver");
             connector =  DriverManager.getConnection("jdbc:mariadb://203.151.92.198/MoveAlarm" +
-                    "?user=ice&password=7571179");
+                    "?user=oat&password=123454322");
             if(connector != null){
+                stmt = connector.createStatement();
                 return true;
             } else {
                 return false;
@@ -73,6 +95,40 @@ public class SQLInquirer {
             e.printStackTrace();
         }
     }
+
+    public HashMap<String,Object> find(int id,String table) throws SQLException {
+        HashMap<String,Object> data = new HashMap<>();
+        stmt = connector.createStatement();
+        String sql = "SELECT * FROM " + table;
+        rs = stmt.executeQuery(sql);
+        ResultSetMetaData rs_m = rs.getMetaData();
+        while(rs != null) {
+            rs.next();
+            if(rs.getInt("id") == id) {
+                for(int col = 1;col <= rs_m.getColumnCount();col++) {
+                    data.put(rs_m.getColumnName(col),rs.getObject(col));
+                }
+                break;
+            }
+        }
+        return data;
+    }
+
+   /* public void save(Object json, String table) throws SQLException {
+        HashMap<String, String> map = new HashMap<String, String>();
+        JSONObject jObject = new JSONObject(t);
+        Iterator<?> keys = jObject.keys();
+
+        while( keys.hasNext() ){
+            String key = (String)keys.next();
+            String value = jObject.getString(key);
+            map.put(key, value);
+
+        }
+        stmt = connector.createStatement();
+        String sql = "INSERT INTO " + table + " (" + column + ") " + "VALUES " + "(" + values + ") ";
+        stmt.execute(sql);
+    }*/
 
     public ArrayList<HashMap<String, Object>> getCollection()
     {
