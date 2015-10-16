1b851d81a726c870f07454d7526ee3c11ce7a818
EnceladusIApetus <moobinkung@windowslive.com>
2015-10-16 12:59:42 +0700

    [#15] model collection interface was created. completely

---
+++ MoveAlarm-Server/src/main/java/movealarm/kmitl/net/Model.java
@@ -0,0 +1,7 @@
+package movealarm.kmitl.net;
+
+/**
+ * Created by Moobi on 16-Oct-15.
+ */
+public interface Model {
+}
--- MoveAlarm-Server/src/main/java/movealarm/kmitl/net/SQLInquirer.java
+++ MoveAlarm-Server/src/main/java/movealarm/kmitl/net/SQLInquirer.java
@@ -1,10 +1,6 @@
 package movealarm.kmitl.net;
 
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
